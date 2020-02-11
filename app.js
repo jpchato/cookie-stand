05187cb066b8432d65a27014d80ad4a3af6a6b7b
Jesse Pena <jpchato@gmail.com>
2020-02-11 14:54:17 -0800

    created constructors with working functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 // var hours is an array of the hours that the store is open
 // var places is an array of the objects
-// var shopData is a function (not a method, because it is not in an object) that eventually should cycle through each hour and generate a number of cookies sold at each location at each hour BUT I HAVE NO IDEA WHAT I'M DOING PLEASE HELP
+// var shopData is a function cycle through each hour and generate a number of cookies sold at each location at each hour
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var places = [seattle, tokyo, dubai, paris, lima];
 
@@ -14,133 +14,6 @@ function randomGuests(min, max){
 
 }
 
-
-// objects, that is `places`, with their associated data
-// name, min, max, avg are all provided
-// storage [] I think has to be used to complete questions 5, 6 of lab
-// randomGuests() method generates a random number of guests between the min and the max. The number can be the min or the max
-// cookiesBought() method generates a random number of cookies bought using the average for each `place` and the randomGuests() method
-var seattle = {
-    name: 'Seattle',
-    min: 23,
-    max: 65,
-    avg: 6.3,
-    cookiesBoughtArrCombo: {
-      byHour: [],
-      sumCookiesBoughtArr: 0,
-    },
-    render: function () {
-      var listEl = document.getElementById('locations');
-      var heading = document.createElement('h2');
-      heading.textContent = this.name;
-      listEl.appendChild(heading);
-
-      for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
-        var itemEl = document.createElement('li');
-        itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
-        listEl.appendChild(itemEl);
-      }
-    }
-}
- 
-
-var tokyo = {
-    name: 'Tokyo',
-    min: 3,
-    max: 24,
-    avg: 1.2,
-    cookiesBoughtArrCombo: {
-      byHour: [],
-      sumCookiesBoughtArr: 0,
-    },
-    render: function () {
-      var listEl = document.getElementById('locations');
-      var heading = document.createElement('h2');
-      heading.textContent = this.name;
-      listEl.appendChild(heading);
-
-      for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
-        var itemEl = document.createElement('li');
-        itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
-        listEl.appendChild(itemEl);
-      }
-    }
-  }
-
-
-var dubai = {
-    name: 'Dubai',
-    min: 11,
-    max: 38,
-    avg: 3.7,
-    cookiesBoughtArrCombo: {
-      byHour: [],
-      sumCookiesBoughtArr: 0,
-    },
-    render: function () {
-      var listEl = document.getElementById('locations');
-      var heading = document.createElement('h2');
-      heading.textContent = this.name;
-      listEl.appendChild(heading);
-
-      for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
-        var itemEl = document.createElement('li');
-        itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
-        listEl.appendChild(itemEl);
-      }
-    }
-  }
-  
-
-var paris = {
-    name: 'Paris',
-    min: 30,
-    max: 28,
-    avg: 2.3, 
-    cookiesBoughtArrCombo: {
-      byHour: [],
-      sumCookiesBoughtArr: 0,
-    },
-    render: function () {
-      var listEl = document.getElementById('locations');
-      var heading = document.createElement('h2');
-      heading.textContent = this.name;
-      listEl.appendChild(heading);
-
-      for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
-        var itemEl = document.createElement('li');
-        itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
-        listEl.appendChild(itemEl);
-      }
-    }
-  }
-
-
-var lima = {
-    name: 'Lima',
-    min: 2,
-    max: 16,
-    avg: 4.6,
-    cookiesBoughtArrCombo: {
-      byHour: [],
-      sumCookiesBoughtArr: 0,
-    },
-    render: function () {
-      var listEl = document.getElementById('locations');
-      var heading = document.createElement('h2');
-      heading.textContent = this.name;
-      listEl.appendChild(heading);
-      
-      for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
-        var itemEl = document.createElement('li');
-        itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
-        listEl.appendChild(itemEl);
-      }
-    }
-}
-
-//calculate the number of cookies sold at each store with the randomGuests variable and the avg of each city
-// stores the TOTAL amount of cookies sold for each hour AND sums the grand total
 function storeData (store){
   var cookiesBoughtArr = [];
   var sumCookiesBoughtArr = 0;
@@ -157,6 +30,48 @@ function storeData (store){
   }
 }
 
+function render(){
+  var listEl = document.getElementById('locations');
+  var heading = document.createElement('h2');
+  heading.textContent = this.name;
+  listEl.appendChild(heading);
+
+  for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
+    var itemEl = document.createElement('li');
+    itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
+    listEl.appendChild(itemEl);
+  }
+}
+
+
+function CookieStand(name, min, max, avg,){
+  this.name=name;
+  this.min=min;
+  this.max=max;
+  this.avg=avg;
+  this.cookiesBoughtArrCombo = {
+    byHour: [],
+    sumCookiesBoughtArr: 0,
+  };
+  this.render= function (){
+    var listEl = document.getElementById('locations');
+    var heading = document.createElement('h2');
+    heading.textContent = this.name;
+    listEl.appendChild(heading);
+
+    for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
+      var itemEl = document.createElement('li');
+      itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
+      listEl.appendChild(itemEl);
+    }
+  };
+}
+var seattle = new CookieStand('Seattle', 23, 65, 6.3)
+var tokyo = new CookieStand('Tokyo', 3, 24, 1.2)
+var dubai = new CookieStand('Dubai', 11, 38, 3.7)
+var paris = new CookieStand('Paris', 30, 28, 2.3)
+var lima = new CookieStand('Lima', 2, 16, 4.6)
+
 seattle.cookiesBoughtArr = storeData(seattle);
 seattle.render();
 paris.cookiesBoughtArr = storeData(paris);
@@ -167,3 +82,135 @@ tokyo.cookiesBoughtArr = storeData(tokyo);
 tokyo.render();
 dubai.cookiesBoughtArr = storeData(dubai);
 dubai.render();
+
+// var seattle = {
+//     name: 'Seattle',
+//     min: 23,
+//     max: 65,
+//     avg: 6.3,
+//     cookiesBoughtArrCombo: {
+//       byHour: [],
+//       sumCookiesBoughtArr: 0,
+//     },
+//     render: function () {
+//       var listEl = document.getElementById('locations');
+//       var heading = document.createElement('h2');
+//       heading.textContent = this.name;
+//       listEl.appendChild(heading);
+
+//       for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
+//         var itemEl = document.createElement('li');
+//         itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
+//         listEl.appendChild(itemEl);
+//       }
+//     }
+// }
+ 
+
+// var tokyo = {
+//     name: 'Tokyo',
+//     min: 3,
+//     max: 24,
+//     avg: 1.2,
+//     cookiesBoughtArrCombo: {
+//       byHour: [],
+//       sumCookiesBoughtArr: 0,
+//     },
+//     render: function () {
+//       var listEl = document.getElementById('locations');
+//       var heading = document.createElement('h2');
+//       heading.textContent = this.name;
+//       listEl.appendChild(heading);
+
+//       for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
+//         var itemEl = document.createElement('li');
+//         itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
+//         listEl.appendChild(itemEl);
+//       }
+//     }
+//   }
+
+// var dubai = {
+//     name: 'Dubai',
+//     min: 11,
+//     max: 38,
+//     avg: 3.7,
+//     cookiesBoughtArrCombo: {
+//       byHour: [],
+//       sumCookiesBoughtArr: 0,
+//     },
+//     render: function () {
+//       var listEl = document.getElementById('locations');
+//       var heading = document.createElement('h2');
+//       heading.textContent = this.name;
+//       listEl.appendChild(heading);
+
+//       for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
+//         var itemEl = document.createElement('li');
+//         itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
+//         listEl.appendChild(itemEl);
+//       }
+//     }
+//   }
+
+
+  
+
+// var paris = {
+//     name: 'Paris',
+//     min: 30,
+//     max: 28,
+//     avg: 2.3, 
+//     cookiesBoughtArrCombo: {
+//       byHour: [],
+//       sumCookiesBoughtArr: 0,
+//     },
+//     render: function () {
+//       var listEl = document.getElementById('locations');
+//       var heading = document.createElement('h2');
+//       heading.textContent = this.name;
+//       listEl.appendChild(heading);
+
+//       for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
+//         var itemEl = document.createElement('li');
+//         itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
+//         listEl.appendChild(itemEl);
+//       }
+//     }
+//   }
+
+
+
+
+// var lima = {
+//     name: 'Lima',
+//     min: 2,
+//     max: 16,
+//     avg: 4.6,
+//     cookiesBoughtArrCombo: {
+//       byHour: [],
+//       sumCookiesBoughtArr: 0,
+//     },
+//     render: function () {
+//       var listEl = document.getElementById('locations');
+//       var heading = document.createElement('h2');
+//       heading.textContent = this.name;
+//       listEl.appendChild(heading);
+      
+//       for (var i=0; i < this.cookiesBoughtArr.byHour.length; i++) {
+//         var itemEl = document.createElement('li');
+//         itemEl.textContent = hours[i] + ' ' + this.cookiesBoughtArr.byHour[i];
+//         listEl.appendChild(itemEl);
+//       }
+//     }
+// }
+
+// //calculate the number of cookies sold at each store with the randomGuests variable and the avg of each city
+// // stores the TOTAL amount of cookies sold for each hour AND sums the grand total
+
+// objects, that is `places`, with their associated data
+// name, min, max, avg are all provided
+// storage [] I think has to be used to complete questions 5, 6 of lab
+// randomGuests() method generates a random number of guests between the min and the max. The number can be the min or the max
+// cookiesBought() method generates a random number of cookies bought using the average for each `place` and the randomGuests() method
+
